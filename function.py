aa9d8a4909cb2c815fa8683786e612988f4306e1
Aidan-Jackson <63417757+Aidan-Jackson@users.noreply.github.com>
2020-04-23 11:25:06 -0700

    Update function.py

--- function.py
+++ function.py
@@ -1,12 +1,48 @@
+test_image = plt.imread('project_image.jpg') #Reads in an example test image
+
 #This function identifies the number, location, and size of white blobs within a single image by
 #using one of the skimage.feature blob detection functions. 
 #Lenses, a white particle on top of a black one, are also counted individually and the same
 #values are tracked. 
-#Function is passed an image [project_image] as a 2D or 3D array, the maximum standard deviation 
+#Function is passed an image [project_image] as a 2D array, the maximum standard deviation 
 # [sig_max] to accept when searching for a blob (which is analogous to maximum allowable 
 # blob size), and the intensity threshold at which it will first consider a blob to be found
 # at [min_thr] computed as a fraction of the maximum intensity in the image. 
 def blobs (project_image,sig_max,min_thr): 
+    
+    #The position, size, and number of blobs in an image
+    
+#Position information is given as the x and y coodinates of the local maximum, assumed to be the
+#center, of each blob found. Size is the average radius of the blob from the center to its  detected edge. 
+
+#Parameters
+#-----------
+#project_image : 2D array
+#               image to be analyzed by the function
+                
+#sig_max : int
+#          maximum standard deviation for use in finding the Gaussian matrix of the image.
+#          Proportional to the maximum allowable size of blobs that can be detected.
+          
+# min_thr : float
+#         minimum intensity threshold for which the function determines the local maxima (center)
+#          of the blob 
+
+#Returns
+#-------
+
+#x : list 
+#             X-Coordinate of detected blobs
+             
+#y : list 
+#             Y-Coordinate of detected blobs
+             
+#r : list 
+#             radius (size) of detected blobs
+           
+#blob_count : int
+#             Number of blobs found in the image
+    
     
     blobs_dog = blob_dog(project_image, max_sigma=sig_max, threshold=min_thr) #uses the 
     #built in 'Blob Detection' function that computes the Laplace of the Gaussian matrix 
@@ -23,9 +59,11 @@ def blobs (project_image,sig_max,min_thr):
     yl = [] #empty list of y coordinates of lenses
     rl = [] #empty list of radius values for lenses
     
+    mean = np.mean(r)
+    
     for i in range(len(r)): #for loop to check each white blob to determine if it
                                          #is also a lens (white on black blob)
-        if r[i-lens_count] < np.mean(r): #checks for a lens by comparing its size to the average of all 
+        if r[i-lens_count] < mean: #checks for a lens by comparing its size to the average of all 
                                  #blobs in the image (lenses can be seen to be smaller on average)
            
             xl.append(x[i-lens_count]) #adds detected lens to its separate list
